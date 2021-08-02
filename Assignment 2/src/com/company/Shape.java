098565038676fa750ba684ed98581db3db632f6a
Ivan Ganatra <ivan.ganatra@gmail.com>
2021-08-02 23:10:41 +0530

    Assignment1 folder added

--- Assignment 2/src/com/company/Shape.java
+++
@@ -1,8 +0,0 @@
-package com.company;
-interface Shape{
-    boolean askInputs();
-    default double calcArea() throws Exception { throw  new Exception("calcArea function should be called for 2D figures only.");}
-    default double calcPerimeter() throws Exception { throw  new Exception("calcArea function should be called for 2D figures only.");}
-    default double calcSurfaceArea() throws Exception { throw  new Exception("calcArea function should be called for 3D figures only.");}
-    default double calcVolume() throws Exception { throw  new Exception("calcArea function should be called for 3D figures only.");}
-}
